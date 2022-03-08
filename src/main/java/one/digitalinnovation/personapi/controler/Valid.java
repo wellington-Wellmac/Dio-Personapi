571efec5c2bde8755fedbc2efa02e8981337a1f8
wellington Machado <dev.wellmac@gmail.com>
2022-03-08 16:31:35 -0300

    Project created

---
+++ src/main/java/one/digitalinnovation/personapi/controler/Valid.java
@@ -0,0 +1,4 @@
+package one.digitalinnovation.personapi.controler;
+
+public @interface Valid {
+}
---
+++ src/test/java/one/digitalinnovation/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonxapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
